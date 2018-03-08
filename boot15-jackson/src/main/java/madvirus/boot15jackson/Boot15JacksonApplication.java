445eb451f71308e1e85db26fe78e124640d85899
madvirus <madvirus@madvirus.net>
2018-03-08 21:41:52 +0900

    config

--- boot15-jackson/src/main/java/madvirus/boot15jackson/Boot15JacksonApplication.java
+++ boot15-jackson/src/main/java/madvirus/boot15jackson/Boot15JacksonApplication.java
@@ -1,12 +1,42 @@
 package madvirus.boot15jackson;
 
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.OffsetDateTimeSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
-public class Boot15JacksonApplication {
+public class Boot15JacksonApplication implements Jackson2ObjectMapperBuilderCustomizer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Boot15JacksonApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Boot15JacksonApplication.class, args);
+    }
+
+    @Override
+    public void customize(Jackson2ObjectMapperBuilder builder) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        LocalDateTimeSerializer localSerializer = new LocalDateTimeSerializer(formatter);
+
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ");
+        CustomOffsetDateTimeSerializer offsetSerializer = new CustomOffsetDateTimeSerializer(formatter2);
+
+        builder
+                .simpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                .serializerByType(LocalDateTime.class, localSerializer)
+                .serializerByType(OffsetDateTime.class, offsetSerializer)
+        ;
+    }
+
+    public class CustomOffsetDateTimeSerializer extends OffsetDateTimeSerializer {
+        public CustomOffsetDateTimeSerializer(DateTimeFormatter formatter) {
+            super(OffsetDateTimeSerializer.INSTANCE, false, formatter);
+        }
+    }
+
 }
--- boot20-jackson/src/main/java/madvirus/boot20jackson/Boot20JacksonApplication.java
+++ boot20-jackson/src/main/java/madvirus/boot20jackson/Boot20JacksonApplication.java
@@ -1,18 +1,41 @@
 package madvirus.boot20jackson;
 
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.OffsetDateTimeSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
 @SpringBootApplication
 public class Boot20JacksonApplication implements Jackson2ObjectMapperBuilderCustomizer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Boot20JacksonApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Boot20JacksonApplication.class, args);
+    }
 
-	@Override
-	public void customize(Jackson2ObjectMapperBuilder builder) {
-	}
+    @Override
+    public void customize(Jackson2ObjectMapperBuilder builder) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        LocalDateTimeSerializer localSerializer = new LocalDateTimeSerializer(formatter);
+
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ");
+        CustomOffsetDateTimeSerializer offsetSerializer = new CustomOffsetDateTimeSerializer(formatter2);
+
+        builder
+                .simpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                .serializerByType(LocalDateTime.class, localSerializer)
+                .serializerByType(OffsetDateTime.class, offsetSerializer)
+        ;
+    }
+
+    public class CustomOffsetDateTimeSerializer extends OffsetDateTimeSerializer {
+        public CustomOffsetDateTimeSerializer(DateTimeFormatter formatter) {
+            super(OffsetDateTimeSerializer.INSTANCE, false, formatter);
+        }
+    }
 }
