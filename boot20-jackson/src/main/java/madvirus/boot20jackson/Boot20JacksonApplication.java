ebd2de93d532757e45a0dea5f4cd7e72f558a427
madvirus <madvirus@madvirus.net>
2018-03-08 18:01:55 +0900

    테스트 중

--- boot20-jackson/src/main/java/madvirus/boot20jackson/Boot20JacksonApplication.java
+++ boot20-jackson/src/main/java/madvirus/boot20jackson/Boot20JacksonApplication.java
@@ -2,11 +2,17 @@ package madvirus.boot20jackson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication
-public class Boot20JacksonApplication {
+public class Boot20JacksonApplication implements Jackson2ObjectMapperBuilderCustomizer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Boot20JacksonApplication.class, args);
+	}
+
+	@Override
+	public void customize(Jackson2ObjectMapperBuilder builder) {
 	}
 }
